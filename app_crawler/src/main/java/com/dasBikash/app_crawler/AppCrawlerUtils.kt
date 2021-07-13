cb499f2fe07546d0a8a6616cc020712278c7883a
Bikash Das <das.bikash.dev@gmail.com>
2021-07-13 19:03:06 +0600

    code clean up.

--- app_crawler/src/main/java/com/dasBikash/app_crawler/AppCrawlerUtils.kt
+++ app_crawler/src/main/java/com/dasBikash/app_crawler/AppCrawlerUtils.kt
@@ -24,11 +24,34 @@ class AppCrawlerUtils {
 
     companion object {
 
-
+        /**
+         * suspend function to launch test
+         *
+         * @param context Android context
+         * @param testSettings object to provide test settings during test launch
+         * @see https://github.com/dasBikash84/app-crawler-model/blob/master/app_crawler_model/src/main/java/com/dasBikash/app_crawler_model/TestSettings.kt
+         *
+         * @return true for test launch success
+         * @author Bikash Das(das.bikash.dev@gmail.com)
+         *
+         * */
         suspend fun startTest(
             context: Context,
             testSettings: TestSettings = TestSettings()
         ):Boolean = RoboAppCrawlerUtils.startTest(context,testSettings)
+
+        /**
+         * async function to launch test
+         *
+         * @param context Android context
+         * @param testSettings object to provide test settings during test launch
+         * @see https://github.com/dasBikash84/app-crawler-model/blob/master/app_crawler_model/src/main/java/com/dasBikash/app_crawler_model/TestSettings.kt
+         *
+         *  @return Task<Boolean>
+         *  [see also](https://github.com/dasBikash84/app-crawler-model/blob/master/app_crawler_model/src/main/java/com/dasBikash/app_crawler_model/Task.kt)
+         *
+         * @author Bikash Das(das.bikash.dev@gmail.com)
+         * */
 
         fun startTestAsync(
             context: Context,
@@ -36,127 +59,12 @@ class AppCrawlerUtils {
         ): Task<Boolean> = RoboAppCrawlerUtils.startTestAsync(context,testSettings)
 
         /**
-         * suspend method to start robo test with a script
-         *
-         *  @param context Android Context
-         *  @param roboScriptPath `Remote/local(Private app storage)/assets` script path
-         *  @param maxRunTimeMinutes Maximum test run time in minutes (>=30 will be truncated to 30)
-         *  @param runOnlyScript whether to run only script or run unscripted robo test
-         *                          also at the end of scripted action execution.
-         *
-         *  @return true if test starts successfully else returns false
-         *
-         * @author Bikash Das(das.bikash.dev@gmail.com)
-         */
-        /*@JvmStatic
-        suspend fun startRoboTestWithScript(
-            context: Context,
-            roboScriptPath: String,
-            maxRunTimeMinutes: Int = 5,
-            runOnlyScript: Boolean = false
-        ): Boolean = RoboAppCrawlerUtils.startTestWithScript(
-            context,
-            roboScriptPath,
-            maxRunTimeMinutes,
-            runOnlyScript
-        )*/
-
-        /**
-         * suspend method to start unscripted robo test
-         *
-         *  @param context Android Context
-         *  @param maxRunTimeMinutes Maximum test run time in minutes (>=30 will be truncated to 30)
-         *
-         *  @return true if test starts successfully else returns false
-         *
-         * @author Bikash Das(das.bikash.dev@gmail.com)
-         */
-        /*@JvmStatic
-        suspend fun startAutoRoboTest(
-            context: Context,
-            maxRunTimeMinutes: Int = 5
-        ): Boolean = RoboAppCrawlerUtils.startAutoTest(
-            context,
-            maxRunTimeMinutes
-        )*/
-
-        /**
-         *  Starts robo test with a script asynchronously
-         *
-         *  @param context Android Context
-         *  @param roboScriptPath `Remote/local(Private app storage)/assets` script path
-         *  @param maxRunTimeMinutes Maximum test run time in minutes  (>=30 will be truncated to 30)
-         *  @param runOnlyScript whether to run only script or run unscripted robo test
-         *                          also at the end of scripted action execution.
-         *
-         *  @return Task<Boolean>
-         *  [see also](https://github.com/dasBikash84/app-crawler-model/blob/master/app_crawler_model/src/main/java/com/dasBikash/app_crawler_model/Task.kt)
-         *
-         * @author Bikash Das(das.bikash.dev@gmail.com)
-         */
-        /*@JvmStatic
-        fun startRoboTestWithScriptAsync(
-            context: Context,
-            roboScriptPath: String,
-            maxRunTimeMinutes: Int = 5,
-            runOnlyScript: Boolean = false
-        ): Task<Boolean> = RoboAppCrawlerUtils.startTestWithScriptAsync(
-            context,
-            roboScriptPath,
-            maxRunTimeMinutes,
-            runOnlyScript
-        )*/
-
-        /**
-         *  Starts unscripted robo test without a script asynchronously
-         *
-         *  @param context Android Context
-         *  @param maxRunTimeMinutes Maximum test run time in minutes  (>=30 will be truncated to 30)
-         *
-         *  @return Task<Boolean>
-         *  [see also](https://github.com/dasBikash84/app-crawler-model/blob/master/app_crawler_model/src/main/java/com/dasBikash/app_crawler_model/Task.kt)
-         *
-         * @author Bikash Das(das.bikash.dev@gmail.com)
-         */
-        /*@JvmStatic
-        fun startAutoRoboTestAsync(
-            context: Context,
-            maxRunTimeMinutes: Int = 5
-        ): Task<Boolean> = RoboAppCrawlerUtils.startAutoTestAsync(
-            context,
-            maxRunTimeMinutes
-        )*/
-
-        /**
          *  Get Interceptor instance to attach on `OkkHttpClient.Builder`
-         *
-         * few calling examples can be as below
-         *
-         * #### To allow only `GET` api calls
-         * ```
-         * OkHttpClient.Builder().addAppCrawlerNetworkInterceptor(RequestMethodFilter.methodGetFilter())
-         *
-         * #### To allow only `GET` and any other api containing '/login' in path
-         * ```
-         * OkHttpClient.Builder().addAppCrawlerNetworkInterceptor(RequestMethodFilter.methodGetFilter(listOf("/login")))
-         * ```
-         * #### To allow only `GET` and `POST` api calls
-         * ```
-         * OkHttpClient.Builder().addAppCrawlerNetworkInterceptor(RequestMethodFilter.methodGetPostFilter())
-         * ```
-         * #### To allow only `GET` , `POST` and `DELETE` api calls
-         * ```
-         * OkHttpClient.Builder().addAppCrawlerNetworkInterceptor(RequestMethodFilter.methodGetPostDeleteFilter())
-         * ```
-         *
-         * With `null` RequestMethodFilter no api call will be restricted.
-         *
-         *  @param requestMethodFilter optional RequestMethodFilter instance to restrict specific api calls
-         *  [see also](https://github.com/dasBikash84/app-crawler-model/blob/master/app_crawler_model/src/main/java/com/dasBikash/app_crawler_model/RequestMethodFilter.kt)
          *
          *  @return Interceptor
          *
          * @author Bikash Das(das.bikash.dev@gmail.com)
+        */
          */
         @JvmStatic
         fun getNetworkTrafficInterceptor()
@@ -215,30 +123,6 @@ class AppCrawlerUtils {
 
 /**
  * Extension on `OkHttpClient.Builder` to attach `AppCrawlerNetworkInterceptor`.
- *
- * few calling examples can be as below
- *
- * #### To allow only `GET` api calls
- * ```
- * OkHttpClient.Builder().addAppCrawlerNetworkInterceptor(RequestMethodFilter.methodGetFilter())
- *
- * #### To allow only `GET` and any other api containing '/login' in path
- * ```
- * OkHttpClient.Builder().addAppCrawlerNetworkInterceptor(RequestMethodFilter.methodGetFilter(listOf("/login")))
- * ```
- * #### To allow only `GET` and `POST` api calls
- * ```
- * OkHttpClient.Builder().addAppCrawlerNetworkInterceptor(RequestMethodFilter.methodGetPostFilter())
- * ```
- * #### To allow only `GET` , `POST` and `DELETE` api calls
- * ```
- * OkHttpClient.Builder().addAppCrawlerNetworkInterceptor(RequestMethodFilter.methodGetPostDeleteFilter())
- * ```
- *
- * With `null` RequestMethodFilter no api call will be restricted.
- *
- *  @param requestMethodFilter optional RequestMethodFilter instance to restrict specific api calls
- *  [see also](https://github.com/dasBikash84/app-crawler-model/blob/master/app_crawler_model/src/main/java/com/dasBikash/app_crawler_model/RequestMethodFilter.kt)
  *
  * */
 fun OkHttpClient.Builder.addAppCrawlerNetworkInterceptor(requestMethodFilter: RequestMethodFilter? = null)
